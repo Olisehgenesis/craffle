482d28d5716026cf2f5125972f22a0915abc607e
Oliseh <studyug@hotmail.com>
2025-05-21 20:14:46 +0300

    pusges

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,9 +1,9 @@
 import { farcasterFrame } from "@farcaster/frame-wagmi-connector";
-import { http, createConfig, injected } from "wagmi";
+import { http, createConfig } from "wagmi";
 import { celo } from "wagmi/chains";
 export const config = createConfig({
   chains: [celo],
-  connectors: [farcasterFrame(), injected()],
+  connectors: [farcasterFrame()],
   transports: {
     [celo.id]: http(),
    
