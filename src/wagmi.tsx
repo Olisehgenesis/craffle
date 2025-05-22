6dfcc841d05f02cd87ef74b5e8c6d508af9ec012
Oliseh <studyug@hotmail.com>
2025-05-22 10:56:11 +0300

    betetr hhs

---
+++ src/wagmi.tsx
@@ -0,0 +1,32 @@
+import { farcasterFrame } from "@farcaster/frame-wagmi-connector";
+import { http, createConfig } from "wagmi";
+import { celo } from "wagmi/chains";
+import { WagmiProvider } from 'wagmi';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactNode } from 'react';
+
+export const config = createConfig({
+  chains: [celo],
+  connectors: [farcasterFrame()],
+  transports: {
+    [celo.id]: http(),
+  },
+});
+
+const queryClient = new QueryClient();
+
+export function WagmiConfig({ children }: { children: ReactNode }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
+
+declare module "wagmi" {
+  interface Register {
+    config: typeof config;
+  }
+}
